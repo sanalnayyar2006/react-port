686020b45a2bf7f556123665b0e4edae27984098
sanalnayyar <sanalsquad@gmail.com>
2025-07-24 15:50:55 +0530

    made the nav bar layout

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+const navbar = () => {
+  return (
+    <div>
+        <nav className='navbar'>
+                <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Contact</a></li>
+                </ul>
+        </nav>
+    </div>
+  )
+} 
+
+export default navbar
